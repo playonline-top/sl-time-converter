23fb012cac97fc4ab2dff59578612be7d6e0a374
Thomas <Thomas@DESKTOP-3UH5TFR>
2024-07-25 15:01:37 +0100

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        ComboBox1 = New ComboBox()
+        TextBox1 = New TextBox()
+        Label1 = New Label()
+        TextBox2 = New TextBox()
+        Label2 = New Label()
+        ComboBox2 = New ComboBox()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        ComboBox1.DropDownStyle = ComboBoxStyle.DropDownList
+        ComboBox1.FlatStyle = FlatStyle.Flat
+        ComboBox1.ForeColor = Color.White
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Items.AddRange(New Object() {"hours", "days", "weeks", "months", "years"})
+        ComboBox1.Location = New Point(68, 12)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(121, 23)
+        ComboBox1.TabIndex = 1
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        TextBox1.BorderStyle = BorderStyle.FixedSingle
+        TextBox1.ForeColor = Color.White
+        TextBox1.Location = New Point(12, 12)
+        TextBox1.MaxLength = 3
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(50, 23)
+        TextBox1.TabIndex = 0
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(12, 41)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(15, 15)
+        Label1.TabIndex = 2
+        Label1.Text = "="
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        TextBox2.BorderStyle = BorderStyle.FixedSingle
+        TextBox2.ForeColor = Color.White
+        TextBox2.Location = New Point(33, 38)
+        TextBox2.Name = "TextBox2"
+        TextBox2.ReadOnly = True
+        TextBox2.Size = New Size(100, 23)
+        TextBox2.TabIndex = 2
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(139, 41)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(50, 15)
+        Label2.TabIndex = 4
+        Label2.Text = "minutes"
+        ' 
+        ' ComboBox2
+        ' 
+        ComboBox2.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        ComboBox2.DropDownStyle = ComboBoxStyle.DropDownList
+        ComboBox2.FlatStyle = FlatStyle.Flat
+        ComboBox2.ForeColor = Color.White
+        ComboBox2.FormattingEnabled = True
+        ComboBox2.Items.AddRange(New Object() {"English", "Magyar", "Deutsch"})
+        ComboBox2.Location = New Point(251, 12)
+        ComboBox2.Name = "ComboBox2"
+        ComboBox2.Size = New Size(121, 23)
+        ComboBox2.TabIndex = 4
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        Button1.FlatStyle = FlatStyle.Popup
+        Button1.Location = New Point(12, 67)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(100, 35)
+        Button1.TabIndex = 3
+        Button1.Text = "Copy"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.Black
+        ClientSize = New Size(384, 111)
+        Controls.Add(Button1)
+        Controls.Add(ComboBox2)
+        Controls.Add(Label2)
+        Controls.Add(TextBox2)
+        Controls.Add(Label1)
+        Controls.Add(TextBox1)
+        Controls.Add(ComboBox1)
+        ForeColor = Color.White
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        MaximizeBox = False
+        Name = "Form1"
+        Text = "SLTimeConverter"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Button1 As Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,75 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ComboBox1.SelectedIndex = 0
+        ComboBox2.SelectedIndex = 0
+    End Sub
+
+    ' Language selector dropdown thingy
+    Private Sub ComboBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox2.SelectedIndexChanged
+        Select Case ComboBox2.SelectedIndex
+            Case 0
+                ComboBox1.Items.Clear()
+                ComboBox1.Items.Add("hours")
+                ComboBox1.Items.Add("days")
+                ComboBox1.Items.Add("weeks")
+                ComboBox1.Items.Add("months")
+                ComboBox1.Items.Add("years")
+                Button1.Text = "Copy"
+                Label2.Text = "minutes"
+
+            Case 1
+                ComboBox1.Items.Clear()
+                ComboBox1.Items.Add("óra")
+                ComboBox1.Items.Add("nap")
+                ComboBox1.Items.Add("hét")
+                ComboBox1.Items.Add("hónap")
+                ComboBox1.Items.Add("év")
+                Button1.Text = "Másolás"
+                Label2.Text = "perc"
+
+            Case 2
+                ComboBox1.Items.Clear()
+                ComboBox1.Items.Add("Stunden")
+                ComboBox1.Items.Add("Tage")
+                ComboBox1.Items.Add("Wochen")
+                ComboBox1.Items.Add("Monate")
+                ComboBox1.Items.Add("Jahre")
+                Button1.Text = "Kopieren"
+                Label2.Text = "Minuten"
+        End Select
+    End Sub
+
+    ' Copy button
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox2.Text.Length = 0 Then Exit Sub
+        My.Computer.Clipboard.SetText(TextBox2.Text)
+    End Sub
+
+    ' the lil text thingy
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        If TextBox1.Text.Length = 0 Then Exit Sub
+        Select Case ComboBox1.SelectedIndex
+            Case 0 ' hours
+                TextBox2.Text = (Convert.ToInt32(TextBox1.Text) * 60).ToString()
+            Case 1 ' days
+                TextBox2.Text = (Convert.ToInt32(TextBox1.Text) * 1440).ToString()
+            Case 2 ' weeks
+                TextBox2.Text = (Convert.ToInt32(TextBox1.Text) * 10080).ToString()
+            Case 3 ' months
+                TextBox2.Text = (Convert.ToInt32(TextBox1.Text) * 43200).ToString()
+            Case 4 ' years
+                TextBox2.Text = (Convert.ToInt32(TextBox1.Text) * 525600).ToString()
+        End Select
+
+        If Convert.ToInt32(TextBox2.Text) > 26297450 Then
+            TextBox2.ForeColor = Color.OrangeRed
+            TextBox2.Text = 26297450
+        Else
+            TextBox2.ForeColor = Color.White
+        End If
+    End Sub
+
+    Private Sub TextBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox1.KeyPress
+        If Not Char.IsNumber(e.KeyChar) And Not Char.IsControl(e.KeyChar) Then e.Handled = True
+    End Sub
+End Class
